4747a34091a8b6d4830437b74c13dfc92fe0de1f
Jesus M Arellano III <74021219+Jarellano562@users.noreply.github.com>
2021-04-25 12:16:28 -0700

    hide footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
