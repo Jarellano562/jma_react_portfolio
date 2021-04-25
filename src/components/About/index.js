9ddd552f7330afd51d6a8e8de69cdcada5dacfa9
Jesus M Arellano III <74021219+Jarellano562@users.noreply.github.com>
2021-04-25 16:15:15 -0700

    updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
 
             <div className="row justify-content-center">
                 <div className="col-10" id="about-section">
-                    <img class="mb-5" src="https://raw.githubusercontent.com/Jarellano562/jma_react_portfolio/main/src/assets/images/me.jpeg" alt="Jesus" />
+                    <img class="mb-5" id="selfie" src="https://raw.githubusercontent.com/Jarellano562/jma_react_portfolio/main/src/assets/images/self.jpeg" alt="Jesus" />
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
         </p>
