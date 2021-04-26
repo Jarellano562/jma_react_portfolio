fc80f4df927092905f204e5e11d5ab9753ce2f95
Jesus M Arellano III <74021219+Jarellano562@users.noreply.github.com>
2021-04-25 21:20:36 -0700

    added update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
             <a href="https://github.com/Jarellano562"><img src="https://img.icons8.com/plasticine/100/000000/github.png" alt="Github" class="icon" /></a>
             <a href="https://www.linkedin.com/in/jesus-m-arellano-iii-7711a923/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon" /></a>
             <p>
-                &copy; Jesus M. Arellano III  |  <a href="https://raw.githubusercontent.com/Jarellano562/jma_react_portfolio/main/src/assets/images/Jesse%20M%20Arellano%20Resume%202021.pdf" class="link">Download Résumé</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">Color Palette</a>  |  Made with: <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React" />
+                &copy; Jesus M. Arellano III  |  <a href="https://raw.githubusercontent.com/Jarellano562/jma_react_portfolio/main/src/assets/images/Jesse%20M%20Arellano%20Resume%202021.pdf" class="link">Download Résumé</a>  |  <a href="https://coolors.co/000000-14213d-fca311-e5e5e5-ffffff">Color Palette</a>  |  Made with: <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React" />
             </p>
 
         </div>
